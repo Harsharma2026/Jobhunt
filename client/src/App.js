60209877ebf18032014e796c072b4b01645353c0
harsharma2026 <harsharma2026@gmail.com>
2023-07-15 21:25:51 +0530

    landing and login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login';
-import Signup from './pages/signup';
+
+import Landing from './pages/landing';
+
+
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        
+        <Route path="/landing" element={<Landing/>} />
+        {/* <Route path="/slider" element={<Slider/>} /> */}
       </Routes>
     </Router>
   );
